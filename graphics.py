c0da1d25b7e0861db8f0e30daac963f71d0dd393
KRothfus <krothfus92@gmail.com>
2025-05-29 06:19:49 -0700

    updated graphics with classes -point class -line class is not finish.

--- graphics.py
+++ graphics.py
@@ -22,3 +22,19 @@ class Window:
 
     def close(self):
         self.__running = False
+
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x #x = 0 is the left side of the screen
+        self.y = y #y = 0 is the top of the screen
+        
+
+class Line:
+    def __init__(self, point1, point2):
+        self.x1, self.y1 = point1
+        self.x2, self.y2 = point2
+        
+    def draw(self, tk_canvas, fill_color): #fill color is a string like 'black'
+        tk_canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2) #how to get create_line to not be white?
+        
