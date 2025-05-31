5d28ec85beb2ed29236b1d005a903cd04a8cd5da
KRothfus <krothfus92@gmail.com>
2025-05-31 15:12:00 -0700

    Printing grid to window working on getting the entrance and exit implemented.

--- graphics.py
+++ graphics.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Tk, BOTH, Canvas
 
 
@@ -43,7 +44,7 @@ class Line:
         canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2) #how to get create_line to not be white? it is supposed to accept a canvas as a parameter.
 
 class Cell:
-    def __init__(self, window):
+    def __init__(self, window=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -59,14 +60,22 @@ class Cell:
         self.__x2 = x2
         self.__y2 = y2
         
-        if self.has_left_wall:
+        if self.has_left_wall and self.__win:
             self.__win.draw_line(Line(Point(self.__x1,self.__y1),Point(self.__x1,self.__y2)),fill_color)
-        if self.has_right_wall:
+        else:
+            self.__win.draw_line(Line(Point(self.__x1,self.__y1),Point(self.__x1,self.__y2)),'white')
+        if self.has_right_wall and self.__win:
             self.__win.draw_line(Line(Point(self.__x2,self.__y1),Point(self.__x2,self.__y2)),fill_color)
-        if self.has_top_wall:
+        else:
+            self.__win.draw_line(Line(Point(self.__x2,self.__y1),Point(self.__x2,self.__y2)),'white')
+        if self.has_top_wall and self.__win:
             self.__win.draw_line(Line(Point(self.__x1,self.__y1),Point(self.__x2,self.__y1)),fill_color)
-        if self.has_bottom_wall:
+        else:
+            self.__win.draw_line(Line(Point(self.__x1,self.__y1),Point(self.__x2,self.__y1)),'white')
+        if self.has_bottom_wall and self.__win:
             self.__win.draw_line(Line(Point(self.__x1,self.__y2),Point(self.__x2,self.__y2)),fill_color)
+        else:
+            self.__win.draw_line(Line(Point(self.__x1,self.__y2),Point(self.__x2,self.__y2)),'white')
     
     def _coordinates_set(self):
         if self.__x1 == -1 or self.__x2 == -1 or self.__y1 == -1 or self.__y2 == -1:
@@ -84,3 +93,50 @@ class Cell:
         to_middle_x = ((to_cell.__x2 - to_cell.__x1)/2)+to_cell.__x1
         to_middle_y = ((to_cell.__y2 - to_cell.__y1)/2)+to_cell.__y1
         self.__win.draw_line(Line(Point(self_middle_x,self_middle_y),Point(to_middle_x,to_middle_y)), fill_color)
+        
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+        ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.__cells = []
+        self.__create_cells()
+    
+    def __create_cells(self):
+        for i in range(self.num_cols):
+            self.__cells.append([])
+            for j in range(self.num_rows):
+                self.__cells[i].append(Cell(self.win))
+                self.__draw_cell(i,j)
+    
+    def __draw_cell(self, i, j):
+        x1 = (self.cell_size_x) * i + self.x1
+        y1 = (self.cell_size_y) * j + self.y1
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        self.__cells[i][j].draw(x1,y1,x2,y2)
+        self.animate()
+    
+    def animate(self):
+        if self.win != None:
+            self.win.redraw()
+            time.sleep(.005)
+            
+    def __break_entrance_and_exit(self):
+        self.__cells[0][0].has_top_wall = False
+        self.__draw_cell(0,0)
+        self.__cells[self.num_rows-1][self.num_cols-1].has_bottom_wall = False
+        self.__draw_cell(self.num_rows-1,self.num_cols-1)
---
+++ tests.py
@@ -0,0 +1,58 @@
+import unittest
+
+from graphics import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_rows,
+        )
+        
+    def test_maze_create_cells100x1(self):
+        num_cols = 1
+        num_rows = 100
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_rows,
+        )
+        
+    def test_maze_create_cells100x100(self):
+        num_cols = 100
+        num_rows = 100
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_create_cells12x0(self):
+        num_cols = 12
+        num_rows = 0
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_rows,
+        )
+if __name__ == "__main__":
+    unittest.main()
