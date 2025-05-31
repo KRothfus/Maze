e590b155a82bdcf269f4d5cfc1bf16d140a08ea1
KRothfus <krothfus92@gmail.com>
2025-05-31 07:04:39 -0700

    created cell and draw_move

--- graphics.py
+++ graphics.py
@@ -39,6 +39,48 @@ class Line:
         self.x2 = point2.x
         self.y2 = point2.y
         
-    def draw(self, canvas, fill_color): #fill color is a string like 'black'
+    def draw(self, canvas, fill_color='green'): #fill color is a string like 'black'
         canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2) #how to get create_line to not be white? it is supposed to accept a canvas as a parameter.
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__y1 = -1
+        self.__x2 = -1
+        self.__y2 = -1
+        self.__win = window
+    def draw(self, x1, y1, x2, y2, fill_color='black'):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
         
+        if self.has_left_wall:
+            self.__win.draw_line(Line(Point(self.__x1,self.__y1),Point(self.__x1,self.__y2)),fill_color)
+        if self.has_right_wall:
+            self.__win.draw_line(Line(Point(self.__x2,self.__y1),Point(self.__x2,self.__y2)),fill_color)
+        if self.has_top_wall:
+            self.__win.draw_line(Line(Point(self.__x1,self.__y1),Point(self.__x2,self.__y1)),fill_color)
+        if self.has_bottom_wall:
+            self.__win.draw_line(Line(Point(self.__x1,self.__y2),Point(self.__x2,self.__y2)),fill_color)
+    
+    def _coordinates_set(self):
+        if self.__x1 == -1 or self.__x2 == -1 or self.__y1 == -1 or self.__y2 == -1:
+            raise Exception('The coordinates are not yet set.')
+    
+    def draw_move(self, to_cell, undo=False):
+        self._coordinates_set()
+        to_cell._coordinates_set()
+        if undo:
+            fill_color = 'gray'
+        else:
+            fill_color = 'red'
+        self_middle_x = ((self.__x2 - self.__x1)/2)+self.__x1
+        self_middle_y = ((self.__y2 - self.__y1)/2)+self.__y1
+        to_middle_x = ((to_cell.__x2 - to_cell.__x1)/2)+to_cell.__x1
+        to_middle_y = ((to_cell.__y2 - to_cell.__y1)/2)+to_cell.__y1
+        self.__win.draw_line(Line(Point(self_middle_x,self_middle_y),Point(to_middle_x,to_middle_y)), fill_color)
